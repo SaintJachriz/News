30393f3f6b4f01f9ac1ecafee34b79a26c906a7b
SaintJachriz <mbagwujachi03@gmail.com>
2024-09-12 14:55:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+function Show() {
+    const hide = document.querySelector('.hidden');
+    hide.style.display = 'flex';
+}
+
+function Hidden() {
+    const hide = document.querySelector('.hidden');
+    hide.style.display = 'none';
+
+}
+
+// let test = document.querySelector('.test-card');
+
+// let card = [{
+//     image: "../news-homepage-main/assets/images/image-retro-pcs.jpg",
+//     title: "01",
+//     subHeader: "Reviving Retro PCs",
+//     text: "What happens when old PCs are given modern upgrade?",
+
+//     image2: "../news-homepage-main/assets/images/image-retro-pcs.jpg",
+//     title2: "02",
+//     subHeader2: "Top 10 Laptops of 2022",
+//     text2: "Our best picks for various needs and budgets",
+
+// }]
+
+// let Test = "";
+// for (let i = 0; i < card.length; i++) {
+
+//     Test = `
+//           <img class="card-img" src="${card[i].image}" alt="">
+//       <div class="card-text">
+//         <h1>${card[i].title}</h1>
+//         <h4>${card[i].subHeader}</h4>
+//         <p>${card[i].text}</p>
+        
+//           <img class="card-img" src="${card[i].image2}" alt="">
+//       <div class="card-text">
+//         <h1>${card[i].title2}</h1>
+//         <h4>${card[i].subHeader2}</h4>
+//         <p>${card[i].text2}</p>
+        
+//         `
+
+//         test.innerHTML +=Test
+// }
+
